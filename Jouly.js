8f85f65fd46893257c5435e7d4a2b8eba658e1ee
TheAnhJouLy <nguyentheanh16052002@gmail.com>
2022-05-20 00:50:16 +0700

    Jouly Bakery

---
+++ Jouly.js
@@ -0,0 +1,29 @@
+$('.main-last-cont').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        992:{
+            items:3
+        }
+    }
+})
+$("#form-footer").validate({
+    rules: { 
+        email: {
+            required: true,
+            email: true,
+        },
+    },  
+    messages: {
+        email: "Please enter a valid email address",
+    }
+});
+
---
+++ remake/common.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+$.ajax({
+        url: 'header.html',
+        cache: false,
+        async: false,
+        success: function(html){
+            $('main').before(html);
+        }
+    });
+
+$.ajax({
+        url: 'footer.html',
+        cache: false,
+        async: false,
+        success: function(html){
+            $('main').after(html);
+        }
+    });	
+})
